585dd8dd6d6470e5243157d1d961757821554cf8
pajakwsei <92627074+pajakwsei@users.noreply.github.com>
2023-10-14 10:31:19 +0200

    Added inital solution

---
+++ LibApp/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace LibApp.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ LibApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
