c65dd59bf37f3b3057373b45724adaf30355530e
pajakwsei <92627074+pajakwsei@users.noreply.github.com>
2023-10-28 10:52:17 +0200

    Added membership type model

--- LibApp/Models/Customer.cs
+++ LibApp/Models/Customer.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public MembershipType MembershipType { get; set; }
         public Customer() 
         { 
         }
---
+++ LibApp/Models/MembershipType.cs
@@ -0,0 +1,10 @@
+﻿namespace LibApp.Models
+{
+    public class MembershipType
+    {
+        public int Id { get; set; }
+        public byte SignUpFee { get; set; }
+        public byte DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+    }
+}
