1d2a8fa85369323aac959850fa4dbaeb2ad15c76
pajakwsei <92627074+pajakwsei@users.noreply.github.com>
2023-10-28 10:34:45 +0200

    Added Customers controller and corresponding views

---
+++ LibApp/Controllers/CustomersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibApp.Models;
+using LibApp.ViewModels;
+
+namespace LibApp.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public ViewResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return Content("User not found");
+            }
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Jan Kowalski" },
+                new Customer { Id = 2, Name = "Monika Nowak" }
+            };
+        }
+    }
+}
