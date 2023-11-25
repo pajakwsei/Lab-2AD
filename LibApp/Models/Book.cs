2b0f8ce55dd147e9a71a3a38968666c8fdeb3674
pajakwsei <92627074+pajakwsei@users.noreply.github.com>
2023-11-25 10:25:51 +0100

    Added genre model and updated Book model

--- LibApp/Models/Book.cs
+++ LibApp/Models/Book.cs
@@ -5,5 +5,10 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
+        public Genre Genre { get; set; }
+        public byte GenreId { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public int NumberInStock { get; set; }
     }
 }
---
+++ LibApp/Models/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibApp.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+    }
+}
