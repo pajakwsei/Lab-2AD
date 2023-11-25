f611d307f286a68ae1aa3af00dc413788af537c4
pajakwsei <92627074+pajakwsei@users.noreply.github.com>
2023-11-25 10:58:07 +0100

    Added ne tables to database

--- LibApp/Data/ApplicationDbContext.cs
+++ LibApp/Data/ApplicationDbContext.cs
@@ -2,6 +2,10 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
+// when having issues with database:
+// 1. Try update-database in NuGet Package Manager Console
+// 2. Delete the original database 
+
 namespace LibApp.Data
 {
     public class ApplicationDbContext : IdentityDbContext
@@ -12,6 +16,8 @@ namespace LibApp.Data
         }
 
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
         public DbSet<MembershipType> MembershipTypes { get; set; }
     }
 }
