1c6f3d3ad280d8fdedb4ea1dbf7d07cf00c5a052
pajakwsei <92627074+pajakwsei@users.noreply.github.com>
2023-11-25 10:20:52 +0100

    Set names of membership types using migration

---
+++ LibApp/Data/Migrations/20231125091206_SetNameOfMembershipTypes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibApp.Data.Migrations
+{
+    public partial class SetNameOfMembershipTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE MembershipTypes SET Name = 'Pay as you go' WHERE Id = 1");
+            migrationBuilder.Sql("UPDATE MembershipTypes SET Name = 'Monthly' WHERE Id = 2");
+            migrationBuilder.Sql("UPDATE MembershipTypes SET Name = 'Quaterly' WHERE Id = 3");
+            migrationBuilder.Sql("UPDATE MembershipTypes SET Name = 'Annually' WHERE Id = 4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
