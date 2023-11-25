517d69813857726cb31b723e3c582bb5e3759631
pajakwsei <92627074+pajakwsei@users.noreply.github.com>
2023-11-25 11:08:04 +0100

    Updated database and seeding

---
+++ LibApp/Data/Migrations/20231125100026_AddBooksAndGenresTablesToDb.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibApp.Data.Migrations
+{
+    public partial class AddBooksAndGenresTablesToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<byte>(type: "tinyint", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GenreId = table.Column<byte>(type: "tinyint", nullable: false),
+                    DateAdded = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    NumberInStock = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Books_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_GenreId",
+                table: "Books",
+                column: "GenreId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+    }
+}
--- LibApp/Models/SeedData.cs
+++ LibApp/Models/SeedData.cs
@@ -12,18 +12,34 @@ namespace LibApp.Models
                     serviceProvider.GetRequiredService<
                         DbContextOptions<ApplicationDbContext>>()))
             {
-                if (context.MembershipTypes.Any())
+                if (!context.MembershipTypes.Any())
                 {
-                    Console.WriteLine("Database already seeded");
-                    return;
+                    context.MembershipTypes.AddRange(
+                        new MembershipType { SignUpFee = 0, DurationInMonths = 0, DiscountRate = 0 },
+                        new MembershipType { SignUpFee = 30, DurationInMonths = 3, DiscountRate = 5 },
+                        new MembershipType { SignUpFee = 90, DurationInMonths = 6, DiscountRate = 10 },
+                        new MembershipType { SignUpFee = 150, DurationInMonths = 12, DiscountRate = 20 }
+                    );
                 }
 
-                context.MembershipTypes.AddRange(
-                    new MembershipType { SignUpFee = 0, DurationInMonths = 0, DiscountRate = 0 },
-                    new MembershipType { SignUpFee = 30, DurationInMonths = 3, DiscountRate = 5 },
-                    new MembershipType { SignUpFee = 90, DurationInMonths = 6, DiscountRate = 10 },
-                    new MembershipType { SignUpFee = 150, DurationInMonths = 12, DiscountRate = 20 }
-                );
+                if (!context.Genres.Any())
+                {
+                    context.Genres.AddRange(
+                        new Genre { Id = 1, Name = "Thriller" },
+                        new Genre { Id = 2, Name = "Document" },
+                        new Genre { Id = 3, Name = "Comedy" }
+                    );
+                }
+
+
+                if (!context.Books.Any())
+                {
+                    context.Books.AddRange(
+                        new Book { Title = "Book 1", Author = "Author 1", GenreId = 1, DateAdded = DateTime.Now, NumberInStock = 3},
+                        new Book { Title = "Book 2", Author = "Author 2", GenreId = 2, DateAdded = DateTime.Now, NumberInStock = 1},
+                        new Book { Title = "Book 3", Author = "Author 3", GenreId = 3, DateAdded = DateTime.Now, NumberInStock = 2}
+                    );
+                }
 
                 context.SaveChanges();
             }
