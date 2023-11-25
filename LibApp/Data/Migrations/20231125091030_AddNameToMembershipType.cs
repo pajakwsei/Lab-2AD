f2c02955e66cff46cd53910ed8a9b88c141a8800
pajakwsei <92627074+pajakwsei@users.noreply.github.com>
2023-11-25 10:11:11 +0100

    Added name to membership type

---
+++ LibApp/Data/Migrations/20231125091030_AddNameToMembershipType.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibApp.Data.Migrations
+{
+    public partial class AddNameToMembershipType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "MembershipTypes",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "MembershipTypes");
+        }
+    }
+}
--- LibApp/Models/MembershipType.cs
+++ LibApp/Models/MembershipType.cs
@@ -1,8 +1,13 @@
-﻿namespace LibApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibApp.Models
 {
     public class MembershipType
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
         public byte SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
