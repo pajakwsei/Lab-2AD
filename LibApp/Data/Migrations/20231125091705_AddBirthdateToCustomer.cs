f371189a31dd33a380da47bbce5ac48a2dd65a9b
pajakwsei <92627074+pajakwsei@users.noreply.github.com>
2023-11-25 10:22:13 +0100

    Migrations omitted during previous commit

---
+++ LibApp/Data/Migrations/20231125091705_AddBirthdateToCustomer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibApp.Data.Migrations
+{
+    public partial class AddBirthdateToCustomer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Birthdate",
+                table: "Customers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthdate",
+                table: "Customers");
+        }
+    }
+}
