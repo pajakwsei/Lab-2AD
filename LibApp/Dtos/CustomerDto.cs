27399cfe675817103353dc890069f8a3973e7420
pajakwsei <92627074+pajakwsei@users.noreply.github.com>
2024-01-13 10:39:06 +0100

    added Customer Data Transfer Object

---
+++ LibApp/Dtos/CustomerDto.cs
@@ -0,0 +1,16 @@
+﻿using LibApp.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace LibApp.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        [StringLength(100)]
+        public string Name { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public int MembershipTypeId { get; set; }
+        public bool HasNewsletterSubscribed { get; set; }
+    }
+}
