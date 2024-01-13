eceb6df594c84215cdacb8b0e9d87da8c40c600c
pajakwsei <92627074+pajakwsei@users.noreply.github.com>
2024-01-13 10:02:16 +0100

    Added anti forgery token validation for CustomersController and CustomerForm

--- LibApp/Controllers/CustomersController.cs
+++ LibApp/Controllers/CustomersController.cs
@@ -49,6 +49,7 @@ namespace LibApp.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Save(Customer customer)
         {
             if (!ModelState.IsValid)
