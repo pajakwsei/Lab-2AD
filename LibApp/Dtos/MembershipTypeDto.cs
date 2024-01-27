3c0e1b56abf50b1efe35e1785ebdffadcb0c1b0b
pajakwsei <92627074+pajakwsei@users.noreply.github.com>
2024-01-27 10:36:34 +0100

    Added membership type dto and mapping

---
+++ LibApp/Dtos/MembershipTypeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace LibApp.Dtos
+{
+    public class MembershipTypeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ LibApp/Profiles/MembershipTypeProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LibApp.Dtos;
+using LibApp.Models;
+
+namespace LibApp.Profiles
+{
+    public class MembershipTypeProfile : Profile
+    {
+        public MembershipTypeProfile() {
+            CreateMap<MembershipType, MembershipTypeDto>();
+            CreateMap<MembershipTypeDto, MembershipType>();
+        }
+    }
+}
